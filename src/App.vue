5cc7f8786779372c64133a300489f1b8657d9b12
xeusteerapat <xeus085@gmail.com>
2021-07-31 14:30:00 +0700

    :tada: finished
    finished resume

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Resume />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Resume from './components/Resume.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Resume,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Inter', sans-serif;
+}
+
+body {
+  background-color: #e2e8f0;
 }
 </style>
---
+++ src/components/Resume.vue
@@ -0,0 +1,288 @@
+<template>
+  <div class="flex items-center justify-center mt-5">
+    <div class="h-auto p-10 bg-indigo-800 w-1/8">
+      <div class="flex items-center justify-center mb-10">
+        <img
+          src="../assets/IMG_5838.jpg"
+          alt="profile"
+          class="w-32 border-2 border-gray-300 rounded-full"
+        />
+      </div>
+      <h1 class="text-lg font-bold tracking-widest text-gray-200 uppercase">
+        Teerapat Prommarak
+      </h1>
+      <hr class="w-1/6 mb-5" />
+      <h1 class="text-lg font-bold tracking-widest text-gray-200 uppercase">
+        Contact
+      </h1>
+      <hr class="w-1/6 mb-5" />
+      <h1 class="text-gray-200 text-md">xeus085@gmail.com</h1>
+      <h1 class="text-gray-200 text-md">Bangkok, Thailand</h1>
+      <a
+        class="text-gray-200 text-md"
+        href="https://curiousdev.in.th/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >https://curiousdev.in.th</a
+      >
+      <div class="flex my-2">
+        <a
+          href="https://www.facebook.com/curiousdevxeus"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Facebook class="my-1 mr-3 text-gray-300 fill-current w-7 h-7" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/teerapat-prommarak"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedinIcon class="my-1 mr-3 text-gray-300 fill-current w-7 h-7" />
+        </a>
+        <a
+          href="https://github.com/xeusteerapat"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GithubIcon class="my-1 text-gray-300 fill-current w-7 h-7" />
+        </a>
+      </div>
+      <hr class="my-5" />
+      <h1
+        class="mt-2 text-lg font-bold tracking-widest text-gray-200 uppercase"
+      >
+        Education
+      </h1>
+      <hr class="w-1/6 mb-5" />
+      <h1 class="font-semibold tracking-wider text-gray-200 uppercase text-md">
+        Full-Stack Javascript
+      </h1>
+      <h1 class="text-gray-300 text-md">
+        <span class="italic">Software Park Code Camp</span>,
+        <span class="italic font-semibold text-gray-200"> Bangkok</span> / 2020
+      </h1>
+      <br />
+      <h1 class="font-semibold tracking-wider text-gray-200 uppercase text-md">
+        Bachelor Degree of Mechanical Engineering
+      </h1>
+      <h1 class="text-gray-300 text-md">
+        <span class="italic">Chiang Mai University</span>,
+        <span class="italic font-semibold text-gray-200"> Chiang Mai</span> /
+        2004 - 2008
+      </h1>
+      <hr class="my-5" />
+      <h1
+        class="mt-2 text-lg font-bold tracking-widest text-gray-200 uppercase"
+      >
+        SKILLS
+      </h1>
+      <hr class="w-1/6 mb-5" />
+      <ul>
+        <div class="my-1">
+          <p class="ml-4 font-semibold text-gray-300 list-none text-md">
+            Frontend
+          </p>
+          <li class="ml-8 text-gray-300 list-disc text-md">HTML/CSS</li>
+          <li class="ml-8 text-gray-300 list-disc text-md">Javascript</li>
+          <li class="ml-8 text-gray-300 list-disc text-md">React</li>
+        </div>
+        <div class="my-1">
+          <li class="ml-4 font-semibold text-gray-300 list-none text-md">
+            Backend
+          </li>
+          <li class="ml-8 text-gray-300 list-disc text-md">Node.js</li>
+          <li class="ml-8 text-gray-300 list-disc text-md">Typescript</li>
+          <li class="ml-8 text-gray-300 list-disc text-md">GraphQL</li>
+        </div>
+        <div class="my-1">
+          <li class="ml-4 font-semibold text-gray-300 list-none text-md">
+            Databases
+          </li>
+          <li class="ml-8 text-gray-300 list-disc text-md">MongoDB</li>
+          <li class="ml-8 text-gray-300 list-disc text-md">PostgresQL</li>
+          <li class="ml-8 text-gray-300 list-disc text-md">MySQL</li>
+        </div>
+        <div class="my-1">
+          <li class="ml-4 font-semibold text-gray-300 list-none text-md">
+            Others
+          </li>
+          <li class="ml-8 text-gray-300 list-disc text-md">AWS</li>
+          <li class="ml-8 text-gray-300 list-disc text-md">Firebase</li>
+          <li class="ml-8 text-gray-300 list-disc text-md">
+            Serverless Framework
+          </li>
+          <li class="ml-8 text-gray-300 list-disc text-md">
+            Git & Github
+          </li>
+        </div>
+      </ul>
+    </div>
+    <div class="w-7/12 h-auto p-10 bg-white">
+      <h1 class="text-lg text-gray-500">
+        Software engineer experienced building EdTech SaaS. Strong in API
+        development and problem-solving skills. Proficient in Node.js,
+        Javascript, and MongoDB with database analysis and design. Eager to
+        learn new technology and build innovative and cutting edge business
+        solutions.
+      </h1>
+      <h1
+        class="mt-6 mb-3 text-2xl font-semibold tracking-wider text-gray-600 uppercase"
+      >
+        Experience
+      </h1>
+      <h1 class="my-1 text-lg font-semibold text-gray-600">
+        Software Engineer - Backend
+      </h1>
+      <h1 class="font-semibold text-gray-600 ">
+        Vonder (Thailand) | (July 2020 - Present)
+      </h1>
+      <ul class="ml-5 text-gray-500 list-disc">
+        <li>
+          Develop application using Node.js and MongoDB with Serverless
+          Framework
+        </li>
+        <li>
+          API Development integrate services with Third-party API (LINE API,
+          Firebase)
+        </li>
+        <li>Application Development with Cloud environment (AWS)</li>
+      </ul>
+      <h1 class="mt-4 mb-1 text-lg font-semibold text-gray-600 ">
+        3D CAD Engineer
+      </h1>
+      <h1 class="font-semibold text-gray-600 ">
+        Samsung Engineering (Thailand) | (Oct 2014 - Jan 2020)
+      </h1>
+      <ul class="ml-5 text-gray-500 list-disc">
+        <li>
+          Setup 3D CAD Software (SmartPlant Spoolgen) according to project
+          requirements.
+        </li>
+        <li>
+          Train and guide 3D CAD users including providing problem-solving of
+          the software.
+        </li>
+        <li>
+          Responsible for checking and preparation of Piping Isometric Drawings
+          using SmartPlant Spoolgen.
+        </li>
+        <li>
+          Coordinating with Engineering and Construction and liaise with the
+          Client on detailed engineering requirements.
+        </li>
+      </ul>
+      <h1 class="mt-4 mb-1 text-lg font-semibold text-gray-600">
+        Research Assistant
+      </h1>
+      <h1 class="font-semibold text-gray-600 ">
+        Mechanical Engineering Department, Chiang Mai University | (Nov 2012 -
+        Apr 2014)
+      </h1>
+      <ul class="ml-5 text-gray-500 list-disc">
+        <li>Research and study in biomass gasification technology.</li>
+        <li>
+          Provide knowledge about the gasification system to be used in the
+          design of prototype.
+        </li>
+        <li>
+          Test the prototype and prepare data for development to a large scale.
+        </li>
+        <li>Checking of the prototype reactor and to resolve the problem.</li>
+      </ul>
+      <h1 class="mt-4 mb-1 text-lg font-semibold text-gray-600">
+        Field Construction Engineer
+      </h1>
+      <h1 class="font-semibold text-gray-600 ">
+        Sino-Thai Public Co., Ltd. | (Apr 2008 - May 2011)
+      </h1>
+      <ul class="ml-5 text-gray-500 list-disc">
+        <li>
+          Supervised structural and piping installation including heavy lifting
+          components.
+        </li>
+        <li>Prepare Technical Query when discrepancies cannot be resolved.</li>
+        <li>
+          Perform basic engineering design, Lifting Equipment design, drawing
+          Designs.
+        </li>
+      </ul>
+      <h1
+        class="mt-6 mb-2 text-2xl font-semibold tracking-wider text-gray-600 uppercase"
+      >
+        Projects
+      </h1>
+      <ul class="ml-0 text-gray-500 list-none">
+        <li>
+          <a
+            href="https://www.vonder.co.th/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="flex"
+            >Micro-Learning Gamification
+            <ExternalLink class="ml-1 text-gray-500 fill-current"
+          /></a>
+        </li>
+        <li>
+          <a
+            href="https://curiousdev.in.th/project"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="flex"
+            >My personal projects
+            <ExternalLink class="ml-1 text-gray-500 fill-current" />
+          </a>
+        </li>
+      </ul>
+      <h1
+        class="mt-6 mb-2 text-2xl font-semibold tracking-wider text-gray-600 uppercase"
+      >
+        LICENSES & CERTIFICATIONS
+      </h1>
+      <ul class="ml-0 text-gray-500 list-none">
+        <li>
+          <a
+            class="flex"
+            href="https://courses.edx.org/certificates/6a5871267a67410b91ee34e177412746"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Introduction to Computer Science and Programming Using Python
+            <ExternalLink class="ml-1 text-gray-500 fill-current" />
+          </a>
+        </li>
+        <li>
+          <a
+            class="flex"
+            href="https://www.coursera.org/account/accomplishments/verify/VK5WKQF6BY7D"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Machine Learning
+            <ExternalLink class="ml-1 text-gray-500 fill-current" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Facebook from './FacebookIcon.vue';
+import LinkedinIcon from './LinkedinIcon.vue';
+import GithubIcon from './GithubIcon.vue';
+import ExternalLink from './ExternalLink.vue';
+
+export default {
+  name: 'Resume',
+  components: {
+    Facebook,
+    LinkedinIcon,
+    GithubIcon,
+    ExternalLink,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
